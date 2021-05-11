c43e9262a2e5c08d69109e0e8de17f6af9e662f1
KeMura-ops <kenti432@yahoo.co.jp>
2021-05-11 13:21:55 +0900

    bootstrapテスト導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import 'bootstrap';
 import '../stylesheets/application';
+//= require jquery
+//= require rails-ujs
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -7,7 +9,7 @@ import '../stylesheets/application';
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -18,3 +20,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function() {
+  $('.a').slick({
+      dots: true,
+  });
+});
